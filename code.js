2bdba9da2d06597cb5e55feb8d5074988f40c323
Aram2142 <andresaaramirez@aol.com>
2021-07-13 16:42:12 -0400

    added close button

--- code.js
+++ code.js
@@ -1,6 +1,8 @@
 const form = document.querySelector("form");
 const input = document.querySelector("#user-input");
 const ul = document.querySelector(".list");
+// const closeBtn = document.querySelector(".close");
+// console.log(closeBtn);
 
 const getValueOfInput = () => {
   if (input.value) {
@@ -9,11 +11,18 @@ const getValueOfInput = () => {
 };
 
 function createLi() {
+  //create li
   const newLi = document.createElement("li");
   const content = document.createTextNode(input.value);
   newLi.appendChild(content);
   ul.appendChild(newLi);
+
+  //create button and append to side of li
+  const button = `<button class="close">X</button>`;
+  newLi.innerHTML += button;
 }
+
+function localStorage() {}
 
 form.addEventListener("submit", (e) => {
   createLi();
