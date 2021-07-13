a951e938e344efae849f1628d8645e6da4d81c1c
Aram2142 <andresaaramirez@aol.com>
2021-07-12 20:58:01 -0400

    messages clears after hitting submit

--- code.js
+++ code.js
@@ -1,15 +1,15 @@
 const form = document.querySelector("form");
 const input = document.querySelector("#user-input");
-console.log(input);
 
-function getValueOfInput() {
-  const detail = input.value;
-  console.log(detail);
-}
+const getValueOfInput = () => {
+  if (input.value) {
+    input.value = "";
+  }
+};
 
 form.addEventListener("submit", (e) => {
   getValueOfInput();
   e.preventDefault();
 });
 
-input.addEventListener("type", getValueOfInput);
+// input.addEventListener("type", getValueOfInput);
